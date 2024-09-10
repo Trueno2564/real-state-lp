3d2f09b68f10f1726f1b3b20e6e9575ce1b0154a
Brian Regalado <brianregalado2564@gmail.com>
2024-09-09 19:59:12 -0500

    crear proyecto inmobiliaria

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter, Quicksand } from 'next/font/google'
+import './globals.css'
+
+const quickSand = Quicksand({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Real State - BEAR.',
+  description: 'Aplicación para una web inmobiliaria',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body className={quickSand.className}>{children}</body>
+    </html>
+  )
+}
