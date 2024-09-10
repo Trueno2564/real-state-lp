4ecc2640ffa43421327ed2218446b048fc5c741e
Brian Regalado <brianregalado2564@gmail.com>
2024-09-09 22:52:17 -0500

    agregar componentes necesarios

--- src/components/TransitionPage/TransitionPage.tsx
+++ src/components/TransitionPage/TransitionPage.tsx
@@ -13,6 +13,15 @@ export function TransitionPage() {
                 transition = {{delay: 0.2, duration:0.6, ease: "easeInOut"}}
                 >
                 </motion.div>
+                <motion.div 
+                className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-secondary/70 opacity-50"
+                variants = {transitionVariantsPage}
+                initial = "initial"
+                animate = "animate"
+                exit = "exit"
+                transition = {{delay: 0.4, duration:0.6, ease: "easeInOut"}}
+                >
+                </motion.div>
             </div>
         </AnimatePresence>
     )
