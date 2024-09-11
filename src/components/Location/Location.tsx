a275fb3febe4d6403261ff84dd360596c5f89a36
Brian Regalado <brianregalado2564@gmail.com>
2024-09-11 17:06:56 -0500

    corregir errores

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -24,9 +24,7 @@ export function Location() {
             <h2 className="max-w-2xl mx-auto my-4 mb-8 text-3xl font-semibold text-center">Disponibles en toda Colombia, con la comunidad mas grande desde 2020</h2>
             <MapContainer center={coordinatePoint} zoom={6} scrollWheelZoom={false} className="h-[700px]">
                 <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"/>
-                <MarkerHouse selectMarker={centerMarket}>
-
-                </MarkerHouse>
+                <MarkerHouse selectMarker={centerMarket}/>
 
             </MapContainer>
         </Transition>
